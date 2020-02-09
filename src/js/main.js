374afc3e891a3046b5c3b74c17b162d106f91b70
Joanna Jerz <joannajerz1@gmail.com>
2020-02-09 16:31:03 +0100

    feat: localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -23,15 +23,29 @@ water_glass.innerHTML=0;
 
 const button_add=document.querySelector('.water-button__add--js');
 const button_remove=document.querySelector('.water-button_remove--js');
+const key = new Date().toISOString().slice(0, 10) 
 
+let glass_value=localStorage.getItem(key);
+if (glass_value != null){
+  water_glass.innerHTML=glass_value;
+}else{
+  water_glass.innerHTML=0;
+}
 
 button_add.addEventListener('click', (e)=>{
   e.preventDefault();
   water_glass.innerHTML=parseInt(water_glass.innerHTML)+1;
+  updateStorage();
 })
 
 button_remove.addEventListener('click', (e)=>{
   e.preventDefault();
   if(parseInt(water_glass.innerHTML)>0){
   water_glass.innerHTML=parseInt(water_glass.innerHTML)-1;
+  updateStorage();
+
 }})
+
+function updateStorage(){
+  localStorage.setItem(key,parseInt(water_glass.innerHTML));
+}
